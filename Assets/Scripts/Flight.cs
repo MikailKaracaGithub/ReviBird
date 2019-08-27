73fefc98cf316431b8fc8e4c6e742ba0c25a78ca
Xavaltir <tombis1999@hotmail.com>
2019-08-27 20:30:59 +0200

    Half the setup

---
+++ Assets/Scripts/Flight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flight : MonoBehaviour
+{
+
+    public float velocity = 1;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 0 means left click
+        {
+            //jump
+            rb.velocity = Vector2.up * velocity;
+
+        }
+    }
+}
